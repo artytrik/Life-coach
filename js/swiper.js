85eb06b5bf8eea59f3c506ef3580c8451a78f62b
artytrik <tema-luch@mail.ru>
2020-04-27 20:04:19 +0300

    Update 2020-04-27T17:04:15.285Z

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var reviewsSwiper = new Swiper(".swiper-container", {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  loop: true
+});
